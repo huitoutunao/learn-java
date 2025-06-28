b807e189b3704af930f981b508a9dece34411a9d
huitoutunao <lh_huitoutunao@163.com>
2025-06-28 21:48:40 +0800

    feat: 新增循环语句

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,41 @@ import java.util.Scanner;
 public class Main {
     // 这是 main 方法，表示程序的主入口
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入数字：");
-        int n = sc.nextInt();
-        if (n > 2) {
-            System.out.println("小伙子不错哦");
+        // for 循环
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
         }
 
-        String val = "拉面";
-        switch(val) {
-            case "面包":
-                System.out.println("面包");
-                break;
-            case "拉面":
-                System.out.println("拉面");
-                break;
-            default:
-                System.out.println("汉堡包");
-                break;
-        }
+        System.out.println(sum);
 
         int count = 1;
-        switch (count) {
-            case 1 -> System.out.println("一");
-            case 2 -> System.out.println("二");
-            case 3 -> System.out.println("三");
-            default -> System.out.println("default");
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
         }
 
-        // 案例
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("请输入数字：");
-        int n2 = sc2.nextInt();
-        switch (n2) {
-            case 1,2,3,4,5:
-                System.out.println("工作日");
-                break;
-            case 6,7:
-                System.out.println("休息日");
-                break;
-            default:
-                System.out.println("没有这个日期");
-                break;
+        // 案例：回文数
+        // 121 就是回文数，从左到右，从右到左读都一样
+        int x = 121;
+        int temp = x;
+        int num = 0;
+        while (x != 0) {
+            int ge = x % 10;
+            x = x / 10;
+            num = num * 10 + ge;
+        }
+        System.out.println(num == temp);
+
+        // 跳过循环
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                // continue; // 结束本次循环，继续下次循环
+                break; // 跳出循环体
+            }
+            System.out.println(i);
         }
     }
 }
